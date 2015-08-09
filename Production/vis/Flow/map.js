ca24c84fdccdf2fa4a6f80090469019f954510d1
Nikita Pestrov <ipestriov@gmail.com>
2015-08-10 01:54:05 +0300

    Added OSM map type

--- Production/vis/Flow/map.js
+++ Production/vis/Flow/map.js
@@ -174,3 +174,22 @@ function pathInRect(d, rect) {
   }
   return false;
 }
+
+//Define OSM map type pointing at the OpenStreetMap tile server
+        // map.mapTypes.set("OSM", new google.maps.ImageMapType({
+        //     getTileUrl: function(coord, zoom) {
+        //         // "Wrap" x (logitude) at 180th meridian properly
+        //         // NB: Don't touch coord.x because coord param is by reference, and changing its x property breakes something in Google's lib
+        //         var tilesPerGlobe = 1 << zoom;
+        //         var x = coord.x % tilesPerGlobe;
+        //         if (x < 0) {
+        //             x = tilesPerGlobe+x;
+        //         }
+        //         // Wrap y (latitude) in a like manner if you want to enable vertical infinite scroll
+        //
+        //         return "http://tile.openstreetmap.org/" + zoom + "/" + x + "/" + coord.y + ".png";
+        //     },
+        //     tileSize: new google.maps.Size(256, 256),
+        //     name: "OpenStreetMap",
+        //     maxZoom: 18
+        // }));
